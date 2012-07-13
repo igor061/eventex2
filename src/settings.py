387a40d345487a27ea6f074f6cd00444cb9fbf1f
Igor061 <igor061@gmail.com>
2012-07-13 11:14:52 -0300

    Retirar o caminho ocmpleto para o DB

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ PROJECT_DIR = Path(__file__).parent
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': PROJECT_DIR.child('database.db'),                      # Or path to database file if using sqlite3.
+        'NAME': 'database.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.

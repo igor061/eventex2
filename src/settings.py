03983379c44e3e31a6304f0de41b91af316bf599
Igor061 <igor061@gmail.com>
2012-07-16 19:31:09 -0300

    Configurando o suso do postgree

--- src/settings.py
+++ src/settings.py
@@ -16,9 +16,9 @@ PROJECT_DIR = Path(__file__).parent
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'eventexdb',                      # Or path to database file if using sqlite3.
-        'USER': 'admin ',                      # Not used with sqlite3.
-        'PASSWORD': 'admin',                  # Not used with sqlite3.
+        'NAME': 'eventex',                      # Or path to database file if using sqlite3.
+        'USER': 'eventex',                      # Not used with sqlite3.
+        'PASSWORD': 'eventex',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }

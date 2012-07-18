a8605f64e3638ba447108e221cc93f8f9db1ac27
Igor061 <igor061@gmail.com>
2012-07-18 12:50:42 -0300

    Habilitacao do Admin e Instalacao do South

--- src/settings.py
+++ src/settings.py
@@ -24,11 +24,11 @@ DATABASES = {'default': dj_database_url.config(default='sqlite:///'+PROJECT_DIR+
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/Sao_Paulo'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 SITE_ID = 1
 
@@ -120,9 +120,11 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+
+    'south',
 
     'src.core',
     'src.subscriptions',
